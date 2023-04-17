dbca8f55755ee76dd73b8748b6acd74db0524639
Oz <osman.13914@gmail.com>
2023-04-17 07:28:32 -0300

    GraphQL Setup

---
+++ src/posts/posts.entity.ts
@@ -0,0 +1,14 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Post{
+
+    @Field((type => Int))
+    id: number
+   
+    @Field()
+    title: string
+   
+    @Field({nullable: true})
+    content?: string
+}
