f42a6fac42838b9f31c8440b71f7c4004fd4e47d
Oz <osman.13914@gmail.com>
2023-04-19 22:29:45 -0300

    Relaciones en GrahpQL

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,21 +1,32 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { PostsService } from './posts.service';
 import { Query } from '@nestjs/graphql';
 import { Post } from './posts.entity';
 import { CreatePostInput } from './dto/create-post-input';
+import { Author } from 'src/authors/entities/author.entity';
 
-@Resolver()
+@Resolver((of)=> Post)
 export class PostsResolver {
 
     constructor(private postsService: PostsService) { }
 
-    @Query((returns)=> [Post])
+    @Query((returns) => [Post])
     posts() {
         return this.postsService.findAll();
     }
 
+    @Query((returns) => Post)
+    post(@Args('id', { type: () => Int }) id: number) {
+        return this.postsService.findProductById(id);
+    }
+
+    @ResolveField((returns) => Author)
+    author(@Parent() post: Post): Promise<Author> {
+        return this.postsService.getAuthor(post.authorId);
+    }
+
     @Mutation((returns) => Post)
-    createPost(@Args('postInput') postInput: CreatePostInput){
+    createPost(@Args('postInput') postInput: CreatePostInput) {
         return this.postsService.createPost(postInput)
     }
 
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,20 +3,36 @@ import { Post } from './posts.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePostInput } from './dto/create-post-input';
+import { Author } from 'src/authors/entities/author.entity';
+import { AuthorsService } from 'src/authors/authors.service';
 
 
 
 @Injectable()
 export class PostsService {
-    constructor(@InjectRepository(Post) private postsRepository: Repository <Post>){}
+    constructor(@InjectRepository(Post) private postsRepository: Repository<Post>,
+        private authorsService: AuthorsService) { }
 
 
-    async findAll(): Promise <Post[]> {
+    async findAll(): Promise<Post[]> {
         return await this.postsRepository.find();
     }
 
-    createPost(post: CreatePostInput): Promise <Post>{
+
+    findProductById(id: number): Promise<Post> {
+        return this.postsRepository.findOne({
+            where: {
+                id,
+            }
+        })
+    }
+
+    createPost(post: CreatePostInput): Promise<Post> {
         const newPost = this.postsRepository.create(post);
         return this.postsRepository.save(newPost)
+    }
+
+    async getAuthor(userId: number): Promise<Author> {
+        return this.authorsService.findOne(userId)
     }
 }
